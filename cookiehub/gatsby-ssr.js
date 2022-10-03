4ecfd745f8e8d533e63beeea019c9390c6b46366
Luca Limardo <lucalimardo@yahoo.it>
2022-10-03 10:40:41 +0200

    fix: added cookiehub id in ci

--- cookiehub/gatsby-ssr.js
+++ cookiehub/gatsby-ssr.js
@@ -1,19 +1,19 @@
 const React = require('react');
 
 exports.onPreRenderHTML = ({ getPostBodyComponents, replacePostBodyComponents }, pluginOptions) => {
-  const { disableOnDev, cookieHubId, categories } = pluginOptions;
+  const { disableOnDev, cookieHubId } = pluginOptions;
 
   if (process.env.NODE_ENV === 'development' && disableOnDev && !cookieHubId) {
     return null;
   }
 
-  const cookiehubScript = React.createElement('script', {
+  const cookiehubComponent = React.createElement('script', {
     key: 'cookiehub',
     src: `https://cookiehub.net/c2/${cookieHubId}.js`,
     async: true
   });
 
-  const postBodyComponentWithCookiehub = [...getPostBodyComponents(), cookiehubScript];
+  const postBodyComponentWithCookiehub = [...getPostBodyComponents(), cookiehubComponent];
 
   replacePostBodyComponents(postBodyComponentWithCookiehub);
 };
